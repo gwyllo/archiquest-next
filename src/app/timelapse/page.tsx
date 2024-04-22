25166c9687d937c34e1d97af13a8f8025d0020cd
Miriam012 <167169471+Miriam012@users.noreply.github.com>
2024-04-22 15:13:52 +1000

    Revert "Update page.tsx"
    This reverts commit 6393e7a2c2dc22b55ffd2a8de001f25fc486d390.

--- src/app/timelapse/page.tsx
+++ src/app/timelapse/page.tsx
@@ -5,13 +5,12 @@ import { describeImagePrompt } from "@/ai/prompts";
 import TagCloud from "@/components/TagCloud";
 import FastImage from "@/components/FastImage";
 
-//An example of using the tag cloud and fast image component to generate a timeline of images depicting changes from 2020 to 2070
+//An example of using the tag cloud and fast image component to generate a timelapse of images
 
 export default function TimelapsePage() {
   const [keywords, setKeywords] = useState<string>("Selected Keywords...");
-  const [year, setYear] = useState<number>(2020); // Start year changed to 2020
+  const [year, setYear] = useState<number>(2024);
   const [animateImages, setAnimateImages] = useState<boolean>(false);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -22,23 +21,19 @@ export default function TimelapsePage() {
             }`}
             onClick={() => setAnimateImages(!animateImages)}
           >
-            {animateImages ? "⏹ Stop Animation" : "▶ Start Animation"}
+            {animateImages ? "⏹" : "▶"}
           </button>
           <TagCloud
-            prompt="Key aspects of urban transformation and climate adaptation"
+            prompt="A cityscape"
             totalTags={20}
             handleSelect={(tags) => setKeywords(tags.join(", "))}
           />
           <FastImage
-            prompt={`Urban and environmental transformation without beef production ${year}, ${keywords}`}
+            prompt={`A cityscape in the year ${year.toString()}, ${keywords}`}
             systemPrompt={describeImagePrompt}
             imageSize="landscape_16_9"
             animate={animateImages ? 5000 : 0}
-            onChange={() => {
-              const nextYear = year + 5;
-              if (nextYear > 2070) setYear(2070); // Cap the year at 2070
-              else setYear(nextYear);
-            }}
+            onChange={(url) => setYear((year) => year + 5)}
           />
         </div>
       </div>
