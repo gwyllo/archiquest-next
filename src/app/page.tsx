02bf6e655dcb398ff6a61044aa530af0a5158399
Miriam012 <167169471+Miriam012@users.noreply.github.com>
2024-04-30 12:36:03 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/timelapse">Timelapse Demo </Link>
-        <Link href="/artcritic">Art Critic Demo </Link>
-        <Link href="/map">Map Demo </Link>
+        <Link href="/timelapse">Fish Farming Simulation </Link>
+
       </div>
     </main>
   );
