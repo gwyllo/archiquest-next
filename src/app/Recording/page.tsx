fd96c58934197b88221d85debe2053430ec65798
ArchieC7 <167300271+ArchieC7@users.noreply.github.com>
2024-06-11 22:42:48 +1000

    Update page.tsx

--- src/app/Recording/page.tsx
+++ src/app/Recording/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Gallery() {
   return (
     <main className="flex flex-col items-center justify-center p-24 bg-cover bg-center">
-      <h1 className="text-4xl font-bold mb-8">Video Gallery</h1>
+      <h1 className="text-4xl font-bold mb-8">Working Code Recording</h1>
       <div className="relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 h-auto">
         <div className="rounded-lg shadow-md w-full overflow-hidden" style={{ paddingTop: '56.25%' /* 16:9 Aspect Ratio */ }}>
           <iframe
