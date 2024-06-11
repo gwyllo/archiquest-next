dc94d31629b33139ece30a03dbfc4190f2d64a5d
ArchieC7 <167300271+ArchieC7@users.noreply.github.com>
2024-06-11 22:26:45 +1000

    adding a youtube video

--- src/app/Recording/page.tsx
+++ src/app/Recording/page.tsx
@@ -5,10 +5,16 @@ export default function Gallery() {
     <main className="flex flex-col items-center justify-center p-24 bg-cover bg-center">
       <h1 className="text-4xl font-bold mb-8">Video Gallery</h1>
       <div className="relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 h-auto">
-        <video className="rounded-lg shadow-md w-full" controls>
-          <source src="/recordingtest1.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <div className="rounded-lg shadow-md w-full overflow-hidden" style={{ paddingTop: '56.25%' /* 16:9 Aspect Ratio */ }}>
+          <iframe
+            className="absolute top-0 left-0 w-full h-full"
+            src="https://youtu.be/ZjBgEkbnX2I?si=GusdBS9Nais9LcfN"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="YouTube Video"
+          ></iframe>
+        </div>
       </div>
     </main>
   );
