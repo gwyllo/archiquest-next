b8d95a3fc7d01ea01c08c5ad6ca097f15a98d33e
ArchieC7 <167300271+ArchieC7@users.noreply.github.com>
2024-06-11 20:42:10 +1000

    another stage of images done

--- src/app/Drawings/page.tsx
+++ src/app/Drawings/page.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image';
 
 const images = [
-    { src: '/earlytimeImage_481.jpg', alt: 'Image 2' },
+    { src: '/earlytimeimage_481.jpg', alt: 'Image 2' },
     { src: '/earlytimePassenger-cruise-ship-Panama-Canal.webp', alt: 'Image 1' },
     { src: '/concept3.png', alt: 'Image 3' },
     { src: '/conceptDALL·E 2024-06-10 01.39.36 - A series of drawings in a flat, minimalist, and retro-futuristic style of an airship labeled Flying Whales lifting cargo containers over a city. The.webp', alt: 'Image 4' },
-    { src: '/Image (3).png', alt: 'Image 2' },
-    { src: '/Imagesunsetpretty.png', alt: 'Image 1' },
-    { src: '/image copy.png', alt: 'Image 3' },
-    { src: '/bigpanama.png', alt: 'Image 4' },
-    { src: '/Image.png', alt: 'Image 2' },
-    { src: '/Imagesunsetpretty.png', alt: 'Image 1' },
+    { src: '/conceptDALL·E 2024-06-10 02.01.12 - A series of drawings in a flat, minimalist, and retro-futuristic style of an airship labeled Flying Whales lifting cargo containers over a city. The.webp', alt: 'Image 4' },
+    { src: '/conceptsections world engine.png', alt: 'Image 2' },
+    { src: '/conceptC0178040-Panama_Canal_cross-section,_artwork.png', alt: 'Image 1' },
+    { src: '/1-2024.png', alt: 'Image 3' },
+    { src: '/2-2024.png', alt: 'Image 4' },
+    { src: '/1-2030.png', alt: 'Image 2' },
+    { src: '/2-2030.png', alt: 'Image 1' },
     { src: '/image copy.png', alt: 'Image 3' },
     { src: '/bigpanama.png', alt: 'Image 4' },
     // Add more images as needed
